4389823789c0d27268f5084d45373fd139047c3f
naruse666 <become15@i.softbank.jp>
2024-03-03 14:31:43 +0900

    temp

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.22.0
 require (
 	github.com/aws/aws-sdk-go-v2 v1.25.2
 	github.com/aws/aws-sdk-go-v2/config v1.27.4
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.30.1
+	github.com/aws/aws-sdk-go-v2/service/rds v1.74.0
 )
 
 require (
@@ -15,7 +15,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.2 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.9.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.20.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.1 // indirect
--- main.go
+++ main.go
@@ -4,34 +4,58 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/rds"
+	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 )
 
 func main() {
-	// Using the SDK's default configuration, loading additional config
-	// and credentials values from the environment variables, shared
-	// credentials, and shared configuration files
+	// Test parameter group name
+	parameterGroupName := "rds-pg"
+
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
-	// Using the Config value, create the DynamoDB client
-	svc := dynamodb.NewFromConfig(cfg)
+	rdsClient := rds.NewFromConfig(cfg)
 
-	// Build the request with its input parameters
-	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
-		Limit: aws.Int32(5),
-	})
+	// Describe before modify
+	describeInput := &rds.DescribeDBParameterGroupsInput{
+		DBParameterGroupName: &parameterGroupName,
+	}
+	var meta *rds.DescribeDBParameterGroupsOutput
+	meta, err = rdsClient.DescribeDBParameterGroups(context.TODO(), describeInput)
 	if err != nil {
-		log.Fatalf("failed to list tables, %v", err)
+		fmt.Printf("Describe DB Parameter Group: %s", err)
+		os.Exit(1)
 	}
 
-	fmt.Println("Tables:")
-	for _, tableName := range resp.TableNames {
-		fmt.Println(tableName)
+	fmt.Printf("ApplyMethod = (%+v) Before modify ", meta.DBParameterGroups)
+	fmt.Printf("ApplyMethod = (%+v) Before modify ", meta.DBParameterGroups)
+	os.Exit(0)
+
+	// Modify
+	var params = []types.Parameter{
+		types.Parameter{
+			ParameterName:  aws.String("log_statement"),
+			ApplyMethod:    "immediate",
+			ParameterValue: aws.String("mod"),
+		},
 	}
+
+	input := &rds.ModifyDBParameterGroupInput{
+		DBParameterGroupName: &parameterGroupName,
+		Parameters:           params,
+	}
+
+	_, err = rdsClient.ModifyDBParameterGroup(context.TODO(), input)
+	if err != nil {
+		fmt.Printf("modifying DB Parameter Group: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("modifying DB Parameter Group are succeeded.")
 }
